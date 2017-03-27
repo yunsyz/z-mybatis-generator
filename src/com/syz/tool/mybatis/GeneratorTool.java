127eb67326f31a8e4afd7ca2491ffcfe4a620e23
yunsyz <yunsyz@163.com>
2017-03-27 09:44:37 +0800

    z-mybatis-generator init

---
+++ src/com/syz/tool/mybatis/GeneratorTool.java
@@ -0,0 +1,40 @@
+package com.syz.tool.mybatis;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.ProgressCallback;
+import org.mybatis.generator.api.VerboseProgressCallback;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GeneratorTool {
+	public static void main(String[] args) {
+		shell();
+	}
+
+	private static void shell() {
+		List<String> warnings = new ArrayList<String>();
+		try {
+			String configFilePath = System.getProperty("user.dir").concat("/resources/generatorConfig.xml");
+			System.out.println("加载配置文件===" + configFilePath);
+			boolean overwrite = true;
+			File configFile = new File(configFilePath);
+			ConfigurationParser cp = new ConfigurationParser(warnings);
+			Configuration config = cp.parseConfiguration(configFile);
+			DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+			MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+			ProgressCallback progressCallback = new VerboseProgressCallback();
+			// myBatisGenerator.generate(null);
+			myBatisGenerator.generate(progressCallback);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (String warning : warnings) {
+			System.out.println(warning);
+		}
+	}
+}
